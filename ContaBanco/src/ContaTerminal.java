c5af6f53b2abb45de72cbf3f7fbfd6e61c48f070
Daniel Arantes <isabelarosa954@gmail.com>
2024-11-02 21:56:03 -0300

    implements class ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World from ContaTerminal file");
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Seja bem vindo(a)!\n" +
+      "Por favor, digite o número da sua conta:");
+    Integer Numero =  Integer.parseInt(scan.nextLine());
+
+    System.out.println("Digite sua agência:");
+    String Agencia = scan.nextLine();
+
+    System.out.println("Ótimo!\nPor gentileza, digite seu nome:");
+    String NomeCliente = scan.nextLine();
+
+    System.out.println("Por último, digite seu saldo:");
+    Float Saldo = Float.parseFloat(scan.nextLine());
+    scan.close();
+
+    String SaidaFormatada = String.format(
+      "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s,"
+      + " conta %d e seu saldo de %.2f já está disponível para saque.", 
+      NomeCliente, Agencia, Numero, Saldo);
+
+    System.out.println(SaidaFormatada);
   }
 }
